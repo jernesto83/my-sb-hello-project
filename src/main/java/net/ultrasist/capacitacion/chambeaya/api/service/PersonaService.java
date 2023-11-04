6dbd9ae5a0e88ec77a6f20df84edf9e6fb98a20d
jernesto83 <contreras.jernesto83@gmail.com>
2023-11-03 22:08:16 -0600

    First commit with baseline

---
+++ src/main/java/net/ultrasist/capacitacion/chambeaya/api/service/PersonaService.java
@@ -0,0 +1,21 @@
+package net.ultrasist.capacitacion.chambeaya.api.service;
+
+import java.util.List;
+
+import net.ultrasist.capacitacion.chambeaya.api.model.Persona;
+
+public interface PersonaService {
+
+	/**
+	 * Regresa una lista de objetos de tipo persona
+	 * @return List<Persona>
+	 */
+	List<Persona> getBolsa();
+	
+	/**
+	 * Calcula el factorial de un entero
+	 * @param n entero al cual se le va a calcular el factorial
+	 * @return entero que tiene el factorial de n
+	 */
+	public int fact(int n);
+}
